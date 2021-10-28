da72fa891070f10bbe7fb4a6ced10c6a5d2bea4f
ss2261 <ss2261@kent.ac.uk>
2021-10-28 11:05:20 +0100

    weathercontroller altered and throughly commented

--- MeetupAPI/Controllers/WeatherForecastController.cs
+++ MeetupAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace MeetupAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]")] //this means that all methods in this controller will be under the name of the controller i.e. weatherforecast
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -23,17 +23,33 @@ namespace MeetupAPI.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        /* this specifies the path after weatherforecast/
+   so it will become weatherforecast/current/value
+        whatever the value being passed is, it will be passed through the method signature 'Get' i.e. [FromRoute] int min
+
+        weatherforecast/current/1?max=20   the ?max=20 is the [FromQuery] this is known as a query string
+        */
+        [HttpGet("current/{min}")] 
+        public IEnumerable<WeatherForecast> Get([FromRoute] int min, [FromQuery]int max )
         {
-            var rng = new Random();
+            var rng = new Random();//creating instance of random object
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
+                TemperatureC = rng.Next(min, max),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+
+
+        /*This is a post request to the api with [FromBody] signature */
+        [HttpPost]
+        public string Post([FromBody] string str) {
+
+            return str;
         }
     }
 }
